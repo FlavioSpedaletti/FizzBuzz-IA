fd92f74fa28688f42cb6b38bc9c2403b667a4a5f
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2025-05-30 09:37:39 -0300

    versão inicial

---
+++ FizzBuzzApp.Tests/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿namespace FizzBuzzApp.Tests;
+
+public class FizzBuzzServiceTests
+{
+    /*private readonly FizzBuzzService _fizzBuzzService;
+
+    public FizzBuzzServiceTests()
+    {
+        _fizzBuzzService = new FizzBuzzService();
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsNotMultipleOf3Or5_ReturnsNumber()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(1);
+        Assert.Equal("1", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3Only_ReturnsFizz()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(3);
+        Assert.Equal("Fizz", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf5Only_ReturnsBuzz()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(5);
+        Assert.Equal("Buzz", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3And5_ReturnsFizzBuzz()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(15);
+        Assert.Equal("FizzBuzz", result);
+    }
+
+    [Theory]
+    [InlineData(2, "2")]
+    [InlineData(4, "4")]
+    [InlineData(6, "Fizz")]
+    [InlineData(9, "Fizz")]
+    [InlineData(10, "Buzz")]
+    [InlineData(20, "Buzz")]
+    [InlineData(30, "FizzBuzz")]
+    public void GetFizzBuzzValue_WithVariousInputs_ReturnsExpectedResults(int input, string expected)
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(input);
+        Assert.Equal(expected, result);
+    }*/
+}
