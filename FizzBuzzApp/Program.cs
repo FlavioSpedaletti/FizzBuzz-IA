13694a5d52ba9675cf299c1f7a2b0c36e6b7f4b0
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2025-06-02 15:11:43 -0300

    versão com mais regras e sem ifs

--- FizzBuzzApp/Program.cs
+++ FizzBuzzApp/Program.cs
@@ -1,17 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// See https://aka.ms/new-console-template for more information
 public class FizzBuzzService
 {
+    private readonly Dictionary<int, string> _rules;
+    
+    public FizzBuzzService()
+    {
+        _rules = new Dictionary<int, string>
+        {
+            { 3, "Fizz" },
+            { 5, "Buzz" },
+            { 7, "Flavio" }
+        };
+    }
+    
+    public FizzBuzzService(Dictionary<int, string> customRules)
+    {
+        _rules = customRules;
+    }
+    
     public string GetFizzBuzzValue(int number)
     {
-        string result = "";
+        // Versão com LINQ (mais avançada, salva como referência):
+        // var ruleTexts = string.Join(" ", 
+        //     _rules.Where(rule => number % rule.Key == 0)
+        //           .Select(rule => rule.Value));
+        // string result = ruleTexts == "" ? number.ToString() : number + " " + ruleTexts;
         
-        if (number % 3 == 0)
-            result += "Fizz";
+        // Versão simples com foreach:
+        string result = number.ToString();
+        foreach (var rule in _rules)
+        {
+            if (number % rule.Key == 0)
+                result += " " + rule.Value;
+        }
         
-        if (number % 5 == 0)
-            result += "Buzz";
-        
-        return result == "" ? number.ToString() : result;
+        return result;
     }
 }
 
@@ -21,7 +48,7 @@ class Program
     {
         var fizzBuzzService = new FizzBuzzService();
         
-        for (int i = 1; i <= 30; i++)
+        for (int i = 1; i <= 105; i++)
         {
             Console.WriteLine(fizzBuzzService.GetFizzBuzzValue(i));
         }
