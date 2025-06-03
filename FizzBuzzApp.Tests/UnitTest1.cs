8541aaebef682ea51fc7dc17c8d767980ace57d4
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2025-06-03 18:10:17 -0300

    ordenação de execução de regras com prioridade

--- FizzBuzzApp.Tests/UnitTest1.cs
+++ FizzBuzzApp.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿namespace FizzBuzzApp.Tests;
+using System.Collections.Generic;
 
 public class FizzBuzzServiceTests
 {
@@ -10,44 +11,97 @@ public class FizzBuzzServiceTests
     }
 
     [Fact]
-    public void GetFizzBuzzValue_WhenNumberIsNotMultipleOf3Or5_ReturnsNumber()
+    public void GetFizzBuzzValue_WhenNumberIsNotMultipleOfAnyRule_ReturnsNumber()
     {
         var result = _fizzBuzzService.GetFizzBuzzValue(1);
         Assert.Equal("1", result);
     }
 
     [Fact]
-    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3Only_ReturnsFizz()
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3Only_ReturnsNumberFizz()
     {
         var result = _fizzBuzzService.GetFizzBuzzValue(3);
-        Assert.Equal("Fizz", result);
+        Assert.Equal("3 Fizz", result);
     }
 
     [Fact]
-    public void GetFizzBuzzValue_WhenNumberIsMultipleOf5Only_ReturnsBuzz()
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf5Only_ReturnsNumberBuzz()
     {
         var result = _fizzBuzzService.GetFizzBuzzValue(5);
-        Assert.Equal("Buzz", result);
+        Assert.Equal("5 Buzz", result);
     }
 
     [Fact]
-    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3And5_ReturnsFizzBuzz()
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf3And5_ReturnsNumberFizzBuzz()
     {
         var result = _fizzBuzzService.GetFizzBuzzValue(15);
-        Assert.Equal("FizzBuzz", result);
+        Assert.Equal("15 FizzBuzz", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf7Only_ReturnsNumberFlavio()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(7);
+        Assert.Equal("7 Flavio", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf21_ReturnsNumberFlavio()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(21);
+        Assert.Equal("21 Flavio", result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf7And3_PrioritizesSmallestExclusiveRule()
+    {
+        // 21 é múltiplo de 3 e 7, mas 7 é exclusivo e menor que 21
+        var result = _fizzBuzzService.GetFizzBuzzValue(21);
+        Assert.Equal("21 Flavio", result); // 7 tem prioridade sobre 21
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_WhenNumberIsMultipleOf7And5_ReturnsOnlyFlavio()
+    {
+        var result = _fizzBuzzService.GetFizzBuzzValue(35);
+        Assert.Equal("35 Flavio", result);
     }
 
     [Theory]
+    [InlineData(1, "1")]
     [InlineData(2, "2")]
+    [InlineData(3, "3 Fizz")]
     [InlineData(4, "4")]
-    [InlineData(6, "Fizz")]
-    [InlineData(9, "Fizz")]
-    [InlineData(10, "Buzz")]
-    [InlineData(20, "Buzz")]
-    [InlineData(30, "FizzBuzz")]
+    [InlineData(5, "5 Buzz")]
+    [InlineData(6, "6 Fizz")]
+    [InlineData(7, "7 Flavio")]
+    [InlineData(9, "9 Fizz")]
+    [InlineData(10, "10 Buzz")]
+    [InlineData(14, "14 Flavio")]
+    [InlineData(15, "15 FizzBuzz")]
+    [InlineData(21, "21 Flavio")]
+    [InlineData(28, "28 Flavio")]
+    [InlineData(30, "30 FizzBuzz")]
     public void GetFizzBuzzValue_WithVariousInputs_ReturnsExpectedResults(int input, string expected)
     {
         var result = _fizzBuzzService.GetFizzBuzzValue(input);
         Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetFizzBuzzValue_ExclusiveRulePriority_SmallerKeyWins()
+    {
+        // Teste para verificar que regras exclusivas são ordenadas por Key (menor tem prioridade)
+        var rules = new List<Rule>
+        {
+            new Rule(21, "Flavioo", true),
+            new Rule(7, "Flavio", true),
+            new Rule(3, "Fizz")
+        };
+        var service = new FizzBuzzService(rules);
+        
+        // 21 é múltiplo de 3, 7 e 21, mas 7 tem menor Key entre as exclusivas
+        var result = service.GetFizzBuzzValue(21);
+        Assert.Equal("21 Flavio", result);
     }
 }
--- FizzBuzzApp/Program.cs
+++ FizzBuzzApp/Program.cs
@@ -3,50 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
+public class Rule
+{
+    public int Key { get; set; }
+    public string Value { get; set; }
+    public bool IsExclusive { get; set; }
+    
+    public Rule(int key, string value, bool isExclusive = false)
+    {
+        Key = key;
+        Value = value;
+        IsExclusive = isExclusive;
+    }
+}
+
 public class FizzBuzzService
 {
-    private readonly Dictionary<int, string> _rules;
+    private readonly List<Rule> _rules;
     
     public FizzBuzzService()
     {
-        _rules = new Dictionary<int, string>
+        _rules = new List<Rule>
         {
-            { 3, "Fizz" },
-            { 5, "Buzz" },
-            { 7, "Flavio" }
+            new Rule(3, "Fizz"),
+            new Rule(5, "Buzz"),
+            new Rule(7, "Flavio", true), // Regra exclusiva
+            new Rule(21, "Flavioo", true) // Regra exclusiva
         };
     }
     
     public FizzBuzzService(Dictionary<int, string> customRules)
+    {
+        _rules = customRules.Select(r => new Rule(r.Key, r.Value)).ToList();
+    }
+    
+    public FizzBuzzService(List<Rule> customRules)
     {
         _rules = customRules;
     }
     
     public string GetFizzBuzzValue(int number)
     {
-        // Versão com LINQ (mais avançada, com prioridade para múltiplos de 7):
-        // if (number % 7 == 0)
-        //     return number + " Flavio";
+        // Versão com LINQ (mais avançada, com prioridade para regras exclusivas):
+        // var exclusiveRule = _rules.Where(rule => rule.IsExclusive && number % rule.Key == 0)
+        //                           .OrderBy(rule => rule.Key)
+        //                           .FirstOrDefault();
+        // if (exclusiveRule != null)
+        //     return number + " " + exclusiveRule.Value;
         // var ruleTexts = string.Join("", 
-        //     _rules.Where(rule => rule.Key != 7 && number % rule.Key == 0)
+        //     _rules.Where(rule => !rule.IsExclusive && number % rule.Key == 0)
         //           .Select(rule => rule.Value));
         // return ruleTexts == "" ? number.ToString() : number + " " + ruleTexts;
         
-        // Prioridade para múltiplos de 7
-        if (number % 7 == 0)
+        // Verificar regras exclusivas primeiro (menor Key tem prioridade)
+        foreach (var rule in _rules.Where(r => r.IsExclusive).OrderBy(r => r.Key))
         {
-            return number + " Flavio";
+            if (number % rule.Key == 0)
+                return number + " " + rule.Value;
         }
         
-        // Versão simples com foreach para outras regras:
-        string result = number.ToString();
-        foreach (var rule in _rules)
+        // Processar regras normais (sem espaços entre elas)
+        string ruleResults = "";
+        foreach (var rule in _rules.Where(r => !r.IsExclusive))
         {
-            if (rule.Key != 7 && number % rule.Key == 0)
-                result += " " + rule.Value;
+            if (number % rule.Key == 0)
+                ruleResults += rule.Value;
         }
         
-        return result;
+        return ruleResults == "" ? number.ToString() : number + " " + ruleResults;
     }
 }
 
@@ -56,7 +80,7 @@ class Program
     {
         var fizzBuzzService = new FizzBuzzService();
         
-        for (int i = 1; i <= 105; i++)
+        for (int i = 1; i <= 30; i++)
         {
             Console.WriteLine(fizzBuzzService.GetFizzBuzzValue(i));
         }
